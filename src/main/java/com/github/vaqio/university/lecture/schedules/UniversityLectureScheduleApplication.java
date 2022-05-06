0efad997d8c4d1165166f7cb418e0744d8aff08d
vaqio <business.vaqio@gmail.com>
2022-05-06 19:50:44 +0200

    Add main spring boot application class

---
+++ src/main/java/com/github/vaqio/university/lecture/schedules/UniversityLectureScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.github.vaqio.university.lecture.schedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityLectureScheduleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UniversityLectureScheduleApplication.class, args);
+  }
+
+}
