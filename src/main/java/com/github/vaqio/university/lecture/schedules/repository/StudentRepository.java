0b742db65c625ed8d5c07f75720e5cc56e0fb48b
vaqio <business.vaqio@gmail.com>
2022-05-06 20:37:28 +0200

    Add student repository implementation

--- src/main/java/com/github/vaqio/university/lecture/schedules/repository/StudentRepository.java
+++ src/main/java/com/github/vaqio/university/lecture/schedules/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.github.vaqio.university.lecture.schedules.repository;
 
-public class StudentRepository {
+import com.github.vaqio.university.lecture.schedules.repository.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
 }
