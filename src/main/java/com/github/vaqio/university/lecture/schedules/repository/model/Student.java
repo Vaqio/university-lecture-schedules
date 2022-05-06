47ae9f3824e5db1f4ba0dbd03741cdc2f4083f55
vaqio <business.vaqio@gmail.com>
2022-05-06 20:24:58 +0200

    Add student model

---
+++ src/main/java/com/github/vaqio/university/lecture/schedules/repository/model/Student.java
@@ -0,0 +1,49 @@
+package com.github.vaqio.university.lecture.schedules.repository.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.vaqio.university.lecture.schedules.repository.model.audit.Auditable;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "students")
+@Entity
+public class Student extends Auditable<Student> {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Length(min = 3, max = 40)
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @NotBlank
+  @Length(min = 3, max = 40)
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
+
+  @NaturalId
+  @NotBlank
+  @Length(min = 3, max = 40)
+  @Email
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @NotBlank
+  @JsonIgnore
+  @Length(min = 3, max = 40)
+  @Column(unique = true)
+  private String password;
+
+}
