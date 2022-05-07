44460178c2096a312183a4bf48781c8d50a5ab7a
vaqio <business.vaqio@gmail.com>
2022-05-07 04:34:56 +0200

    Add lecture room dto

---
+++ src/main/java/com/github/vaqio/university/lecture/schedules/controller/dto/LectureRoomDTO.java
@@ -0,0 +1,25 @@
+package com.github.vaqio.university.lecture.schedules.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Null;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LectureRoomDTO {
+
+  @Null
+  private Long id;
+
+  @JsonProperty("room-number")
+  @NotEmpty
+  @Length(min = 3, max = 40)
+  private String roomNumber;
+
+}
