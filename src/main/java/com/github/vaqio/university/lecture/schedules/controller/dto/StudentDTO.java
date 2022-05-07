1f4827c40ffd383da249423c4dc5bda1b08980be
vaqio <business.vaqio@gmail.com>
2022-05-07 04:26:29 +0200

    Add student dto

---
+++ src/main/java/com/github/vaqio/university/lecture/schedules/controller/dto/StudentDTO.java
@@ -0,0 +1,40 @@
+package com.github.vaqio.university.lecture.schedules.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Null;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDTO {
+
+  @Null
+  private Long id;
+
+  @JsonProperty("first-name")
+  @NotEmpty
+  @Length(min = 3, max = 40)
+  private String firstName;
+
+  @JsonProperty("last-name")
+  @NotEmpty
+  @Length(min = 3, max = 40)
+  private String lastName;
+
+  @NotEmpty
+  @Length(min = 3, max = 40)
+  @Email
+  private String email;
+
+  @NotEmpty
+  @Length(min = 3, max = 40)
+  private String password;
+
+}
