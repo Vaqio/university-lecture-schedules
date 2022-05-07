6fb61a9ebdeb2cc5d49e191363558c890c8eb5db
vaqio <business.vaqio@gmail.com>
2022-05-07 04:32:50 +0200

    Add subject service

---
+++ src/main/java/com/github/vaqio/university/lecture/schedules/service/SubjectService.java
@@ -0,0 +1,31 @@
+package com.github.vaqio.university.lecture.schedules.service;
+
+import com.github.vaqio.university.lecture.schedules.controller.dto.SubjectDTO;
+import com.github.vaqio.university.lecture.schedules.repository.model.Subject;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubjectService {
+
+  boolean existsById(Long id);
+
+  boolean existsByName(String name);
+
+  void create(SubjectDTO subjectDTO);
+
+  void update(Long id, SubjectDTO subjectDTO);
+
+  void deleteById(Long id);
+
+  void deleteByName(String name);
+
+  void deleteAll();
+
+  Optional<Subject> getById(Long id);
+
+  Optional<Subject> getByName(String name);
+
+  List<Subject> getAll();
+
+}
