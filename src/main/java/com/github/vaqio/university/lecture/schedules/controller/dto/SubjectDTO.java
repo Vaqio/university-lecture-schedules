75cc6f5c0507f2ce6392048e717d2e8a5de4c405
vaqio <business.vaqio@gmail.com>
2022-05-07 04:34:45 +0200

    Change class access

--- src/main/java/com/github/vaqio/university/lecture/schedules/controller/dto/SubjectDTO.java
+++ src/main/java/com/github/vaqio/university/lecture/schedules/controller/dto/SubjectDTO.java
@@ -1,10 +1,16 @@
 package com.github.vaqio.university.lecture.schedules.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Null;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubjectDTO {
 
   @Null
