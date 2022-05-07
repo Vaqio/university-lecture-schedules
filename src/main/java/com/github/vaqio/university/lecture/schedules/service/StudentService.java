392fbbcc926b4b4c482f62bc9a3da6189b5d68f4
vaqio <business.vaqio@gmail.com>
2022-05-07 04:29:42 +0200

    Add student service

---
+++ src/main/java/com/github/vaqio/university/lecture/schedules/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.github.vaqio.university.lecture.schedules.service;
+
+import com.github.vaqio.university.lecture.schedules.controller.dto.StudentDTO;
+import com.github.vaqio.university.lecture.schedules.repository.model.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentService {
+
+  boolean existsId(Long id);
+
+  boolean existsEmail(String email);
+
+  void create(StudentDTO studentDTO);
+
+  void update(Long id, StudentDTO studentDTO);
+
+  void deleteById(Long id);
+
+  void deleteByEmail(String email);
+
+  void deleteAll();
+
+  Optional<Student> getById(Long id);
+
+  Optional<Student> getByEmail(String email);
+
+  List<Student> getAll();
+
+}
