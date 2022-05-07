0bc5ca45d9c4d7e193fe7b7ba25679d9183dbcf3
vaqio <business.vaqio@gmail.com>
2022-05-07 04:36:27 +0200

    Add lecture room service

---
+++ src/main/java/com/github/vaqio/university/lecture/schedules/service/LectureRoomService.java
@@ -0,0 +1,31 @@
+package com.github.vaqio.university.lecture.schedules.service;
+
+import com.github.vaqio.university.lecture.schedules.controller.dto.LectureRoomDTO;
+import com.github.vaqio.university.lecture.schedules.repository.model.LectureRoom;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LectureRoomService {
+
+  boolean existsById(Long id);
+
+  boolean existsByRoomNumber(String roomNumber);
+
+  void create(LectureRoomDTO lectureRoomDTO);
+
+  void update(Long id, LectureRoomDTO lectureRoomDTO);
+
+  void deleteById(Long id);
+
+  void deleteByRoomNumber(String roomNumber);
+
+  void deleteAll();
+
+  Optional<LectureRoom> getById(Long id);
+
+  Optional<LectureRoom> getByRoomNumber(String roomNumber);
+
+  List<LectureRoom> getAll();
+
+}
